f2c7ac00a6bdf15479c5ec05c1ad62339194d35a
Gladiator <fightfreeeak@gmail.com>
2021-08-23 23:35:58 +0530

    responsive commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+  function on() {
+    document.getElementById("small-screen-menu").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("small-screen-menu").style.display = "none";
+  } 
